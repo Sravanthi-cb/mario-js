275f9c7224e74cc955ac93899a8159e69c5956b5
Lahari <Lahari>
2017-03-14 15:44:24 -0400

    refactor to use jQuery

--- mario.js
+++ mario.js
@@ -1,5 +1,3 @@
-
-
 // set a handler function for the form's submission event
 $("#draw-form").submit(function(event) {
 
@@ -11,7 +9,8 @@ $("#draw-form").submit(function(event) {
 
     // TODO 3
     // figure out the height the user typed (replace the "5" below)
-    heightStr = "5";
+    // heightStr = "5";
+       heightStr = $("#height").val();
 
     // if they didn't type anything, yell at them and exit early
     if (heightStr == "") {
@@ -48,9 +47,11 @@ $("#draw-form").submit(function(event) {
 function displayError(message) {
     // TODO 4
     // implement this function using jQuery
+    $("#pyramid").empty();
+    $("#height").addClass("invalid-field");
+    $(".error-message").text(message);
 
 }
-
 
 /*
  * clearError
@@ -73,7 +74,7 @@ function drawPyramid(height) {
 
     // TODO 2
     // clear the old content from the #pyramid container
-
+      $("#pyramid").empty();
 
     // for each row....
     for (var row = 0; row < height; row++) {
@@ -97,6 +98,7 @@ function drawPyramid(height) {
 
         // TODO 1
         // insert the paragraph into the #pyramid container
-
+        rowElem = $("<p>").html(rowStr);
+        $("#pyramid").append(rowElem);
     }
 }
